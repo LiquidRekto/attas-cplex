0110a8810b7c24d96dcd3bc80ccc26b1168df83c
LiquidRekto <dungliqrekt@gmail.com>
2023-03-08 17:06:54 +0700

    First commit

---
+++ ATTAS_CPLEX/ATTASCplexProgram.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ILOG.Concert;
+using ILOG.CPLEX;
+
+namespace ATTAS_CPLEX
+{
+    public static class ATTASCplexProgram
+    {
+        public static void Execute(Data data)
+        {
+            if (!data.isInitialzied())
+            {
+                throw new System.Exception("Data is not initialized!");
+            }
+
+            IModeler model;
+
+            Cplex cp = new Cplex();
+
+            // objectives
+            /*
+            // Objective 2
+            IObjective obj2 = cp.Minimize();
+            obj2.Name = "SubjectDiversity";
+            obj2.Sense = ObjectiveSense.Minimize;
+
+            List<INumExpr> weighted22 = new List<INumExpr>();
+            List<INumExpr> weighted2 = new List<INumExpr>();
+
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                List<INumExpr> subjDiversity = new List<INumExpr>();
+                for (int c = 0; c < data.Courses; c++)
+                {
+                    
+                    List<INumExpr> exprs = new List<INumExpr>();
+                    for (int t = 0; t < data.Tasks; t++)
+                    {
+                        if (data.TaskCourse[t,c] == 1)
+                        {
+                            weighted2.Add(data.LecturerAssignment[l, t]);
+                        }
+                        data.LecturerCourseStatus.Add((l, c),cp.BoolVar($"l{l}c{c}"));
+                        cp.IfThen(cp.AddGe(data.LecturerCourseStatus[(l, c)],1), cp.AddGe(cp.Sum(weighted2.ToArray()), 1));
+                        cp.IfThen(cp.AddEq(data.LecturerCourseStatus[(l, c)], 0), cp.AddEq(cp.Sum(weighted2.ToArray()), 0));
+                        exprs.Add(data.LecturerCourseStatus[(l, c)]);
+                    }
+                    subjDiversity.Add(cp.Sum(exprs.ToArray()));
+
+                }
+                INumVar obj = cp.NumVar(0, data.Courses, NumVarType.Int, "subjectDiversity");
+                weighted22.Add(cp.AddEq(obj, cp.Max(subjDiversity.ToArray())));
+            }
+            obj2.Expr = cp.Sum(weighted22.ToArray());
+            */
+            // Objective 3
+            IObjective obj3 = cp.Minimize();
+            obj3.Name = "QuotaReached";
+            obj3.Sense = ObjectiveSense.Minimize;
+
+            List<INumExpr> weighted3 = new List<INumExpr>();
+
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                INumExpr[] diffExpr = new INumExpr[data.Tasks];
+                for (int t = 0; t < data.Tasks; t++)
+                {
+                    diffExpr[t] = cp.Sum(data.LecturerAssignment[l, t],-data.LecturerQuota[l]);
+                }
+                weighted3.Add(cp.Abs(cp.Sum(diffExpr)));
+            }
+            obj3.Expr = cp.Sum(weighted3.ToArray());
+
+            // Objective 5
+            IObjective obj5 = cp.Maximize();
+            obj5.Name = "SubjectPreference";
+            obj5.Sense = ObjectiveSense.Maximize;
+
+            List<INumExpr> weighted5 = new List<INumExpr>();
+            for (int t = 0; t < data.Tasks; t++)
+            {
+
+                // Sum section
+                for (int c = 0; c < data.Courses; c++)
+                {
+                    for (int l = 0; l < data.Lecturers; l++)
+                    {
+                        weighted5.Add(cp.Prod(data.LecturerCoursePref[l, c] * data.TaskCourse[t, c], data.LecturerAssignment[l, t]));
+                    }
+                }
+
+            }
+            obj5.Expr = cp.Sum(weighted5.ToArray());
+
+
+            // Objective 6
+            IObjective obj6 = cp.Maximize();
+            obj6.Name = "SlotPreference";
+            obj6.Sense = ObjectiveSense.Maximize;
+
+            List<INumExpr> weighted6 = new List<INumExpr>();
+            for (int t = 0; t < data.Tasks; t++)
+            {
+                
+                // Sum section
+                for (int s = 0; s < data.Slots; s++)
+                {
+                    for (int l = 0; l < data.Lecturers; l++)
+                    {
+                        weighted6.Add(cp.Prod(data.LecturerSlotPreference[l, s] * data.TaskSlot[t,s], data.LecturerAssignment[l, t]));
+                    }
+                }
+                
+            }
+            obj6.Expr = cp.Sum(weighted6.ToArray());
+            // Weighted sum for objectives
+
+            // staticLex or just combine all of them altogether????
+            INumExpr[] objectivesArr = new INumExpr[]
+            {
+                obj3.Expr,
+                obj5.Expr,
+                obj6.Expr
+            };
+
+            // Adding to model
+            cp.Add(cp.Maximize(cp.StaticLex(objectivesArr)));
+
+            // subjects to (constraints)
+
+            // Constraint 0-0
+            for (int k = 0; k < data.Tasks; k++)
+            {
+                INumVar[] lecTask = new INumVar[data.Lecturers];
+                for (int i = 0; i < data.Lecturers; i++)
+                {
+                    lecTask[i] = data.LecturerAssignment[i, k];
+                }
+                cp.AddEq(cp.Sum(lecTask),1);
+            }
+            
+            //
+            
+            // Constraint 0-1
+            for (int i = 0; i < data.Lecturers; i++)
+            {
+                INumVar[] lecTask = new INumVar[data.Tasks];
+                for (int k = 0; k < data.Tasks; k++)
+                {
+                    lecTask[k] = data.LecturerAssignment[i, k];
+                }
+                cp.AddLe(cp.Sum(lecTask), data.LecturerQuota[i]);
+            }
+
+            // Constraint 1
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                for (int j = 0; j < data.Slots; j++)
+                {
+                    INumExpr[] exprs = new INumExpr[data.Slots*data.Tasks];
+                    // Sum area
+                    for (int t = 0; t < data.Tasks; t++)
+                    {
+                        for (int i = 0; i < data.Slots; i++)
+                        {
+                            //Console.WriteLine("Now exec at row {0}, col {1}, block: {2}",t,i, t * data.Tasks + i);
+
+                            exprs[t*data.Slots+i] = cp.Prod(data.TaskSlot[t, i] * data.SlotConflict[i, j], data.LecturerAssignment[l, t]);
+                        }
+                    }
+                    
+                    cp.AddLe(cp.Sum(exprs), 1);
+                }
+            }
+
+            // Constraint 2
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                for (int t = 0; t < data.Tasks; t++)
+                {
+                    cp.AddRange(0,cp.Sum(data.LecturerAssignment[l, t], -data.LecturerPreAssign[l, t]),1);
+                }
+            }
+
+            // Constraint 3
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                INumExpr[] exprs = new INumExpr[data.Courses * data.Tasks];
+                // Sum section
+                for (int t = 0; t < data.Tasks; t++)
+                {
+                    for (int c = 0; c < data.Courses; c++)
+                    {
+                        exprs[t * data.Courses + c] = cp.Prod((int)Math.Abs(1.0 - data.LecturerCourseAvailability[l, c]) * data.TaskCourse[t, c], data.LecturerAssignment[l, t]);
+                    }
+                }
+                cp.AddEq(cp.Sum(exprs),0);
+            }
+
+            // Constraint 4
+            for (int l = 0; l < data.Lecturers; l++)
+            {
+                INumExpr[] exprs = new INumExpr[data.Slots * data.Tasks];
+                // Sum section
+                for (int t = 0; t < data.Tasks; t++)
+                {
+                    for (int s = 0; s < data.Slots; s++)
+                    {
+                        exprs[t * data.Slots + s] = cp.Prod((int)Math.Abs(1.0 - data.LecturerSlotAvailability[l, s]) * data.TaskSlot[t, s], data.LecturerAssignment[l, t]);
+                    }
+                }
+                cp.AddEq(cp.Sum(exprs), 0);
+            }
+
+            if (cp.Solve())
+            {
+                Console.WriteLine("Objective: {0}", cp.GetObjValue());
+                for (int i = 0; i < data.LecturerAssignment.GetLength(0); i++)
+                {
+                    for (int k = 0; k < data.LecturerAssignment.GetLength(1); k++)
+                    {
+                        if (cp.GetValue(data.LecturerAssignment[i, k]) == 1) {
+                            Console.WriteLine("Task {0} assigned to instructor {1}",k,i);
+                        }
+                        
+                    }
+                   // Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ ATTAS_CPLEX/Data.cs
@@ -0,0 +1,127 @@
+﻿using ILOG.Concert;
+using ILOG.CPLEX;
+
+namespace ATTAS_CPLEX
+{
+    public class Data
+    {
+        bool initialized = false;
+        Cplex cp = new Cplex();
+        // Static vars
+        public int Lecturers;             // L
+        public int Courses;               // C
+        public int Rooms;                 // R
+        public int Slots;                 // S
+        public int Tasks;                 // T
+        public int BackupLecturers;
+
+        // Matrix data
+        public int[,]? LecturerCourseAvailability;               // P
+        public int[,]? LecturerSlotAvailability;                 // F
+        public int[,]? TaskCourse;                               // X
+        public int[,]? TaskSlot;                                 // Y
+        public int[,]? LecturerSlotPreference;                   // M
+        public int[,]? LecturerCoursePref;                       // N
+        public int[,]? LecturerPreAssign;                        // O
+        public int[,]? SlotCompatibility;                        // G
+        public int[,]? AreaDistance;                             // E
+        public int[,]? AreaSlotWeight;                           // Q
+        public int[,]? SlotConflict;                             // D
+        public int[]? LecturerQuota;                              // K
+
+        
+        public Dictionary<(int,int), IIntVar> LecturerCourseStatus;
+
+        public List<(int, int, int)> LecturerPreassign { get; set; } = new List<(int, int, int)>();
+        // Mappings
+        public int[]? taskSubjectMapping;
+        public int[]? taskSlotMapping;
+        public int[]? taskAreaMapping;
+
+       
+
+        // Decision variables?
+        public INumVar[,] LecturerAssignment;                    // A
+
+        public Data(int Lecturers, int Courses, int Rooms, int Slots, int Tasks)
+        {
+            
+            this.Lecturers = Lecturers;             
+            this.Courses = Courses;               
+            this.Rooms = Rooms;
+            this.Slots = Slots;                
+            this.Tasks = Tasks;
+
+            this.InitMatricies();
+        }
+        
+        private void InitMatricies()
+        {
+            LecturerCourseAvailability = new int[this.Lecturers, this.Courses];              // P
+            LecturerSlotAvailability = new int[this.Lecturers, this.Slots];                // F
+            TaskCourse = new int[this.Tasks, this.Courses];                              // X
+            TaskSlot = new int[this.Tasks, this.Slots];                                // Y
+            LecturerSlotPreference = new int[this.Lecturers, this.Slots];                  // M
+            LecturerCoursePref = new int[this.Lecturers, this.Courses];                      // N
+            LecturerPreAssign = new int[this.Lecturers, this.Tasks];                       // O
+            SlotCompatibility = new int[this.Slots, this.Slots];                       // G
+            AreaDistance = new int[this.Rooms, this.Rooms];                            // E
+            AreaSlotWeight = new int[this.Slots, this.Slots];                          // Q
+            SlotConflict = new int[this.Slots, this.Slots];                            // D
+            LecturerQuota = new int[this.Lecturers];
+
+            LecturerCourseStatus = new Dictionary<(int, int), IIntVar>();
+
+        LecturerAssignment = new INumVar[this.Lecturers, this.Tasks];
+            initialized= true;
+        }
+
+        public bool isInitialzied()
+        {
+            return initialized;
+        }
+
+        public void ImportMatrixData(int[,] targetVar,params int[] data)
+        {
+            //Console.WriteLine("Row Length: {0}", targetVar.GetLength(0));
+            //Console.WriteLine("Col Length: {0}", targetVar.GetLength(1));
+            if (data.Length < targetVar.GetLength(0)*targetVar.GetLength(1))
+            {
+                throw new System.Exception("Input data size mismatch! Required " + targetVar.GetLength(0) * targetVar.GetLength(1) + " inputs but you only input " + data.Length);
+            }
+            for (int i = 0; i < targetVar.GetLength(0); i++)
+            {
+                for (int k = 0; k < targetVar.GetLength(1); k++)
+                {
+                    //Console.WriteLine("Imported: {0} to row {1}, col {2} - data pos: {3}", data[i * targetVar.GetLength(1) + k], i, k, i * targetVar.GetLength(1) + k);
+                    targetVar[i, k] = data[i * targetVar.GetLength(1) + k];
+                }
+            }
+        }
+
+        public void ImportArrayData(int[] targetVar, params int[] data)
+        {
+            if (data.Length < targetVar.Length)
+            {
+                throw new System.Exception("Input data size mismatch! Required " + targetVar.GetLength(0) * targetVar.GetLength(1) + " inputs but you only input " + data.Length);
+            }
+            for (int i = 0; i < targetVar.Length; i++)
+            {
+                targetVar[i] = data[i];
+            }
+        }
+
+        public void FillDecisionVarMatrix()
+        {
+            for (int i = 0; i < this.LecturerAssignment.GetLength(0); i++)
+            {
+                for (int k = 0; k < this.LecturerAssignment.GetLength(1); k++)
+                {
+                    this.LecturerAssignment[i, k] = cp.NumVar(0, 1, NumVarType.Int);
+                }
+            }
+        }
+
+
+    }
+}
